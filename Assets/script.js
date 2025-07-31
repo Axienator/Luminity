b7c58c788106d9144e8660c324bc749de9bedeb2
paullorenzopsima@gmail.com <jericho@gestafi.com>
2025-07-31 21:55:05 +0800

    Add restart button

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,15 @@
 const timerdisplay = document.querySelector('#timer')
 const controls = document.querySelector('.control')
+const startpausetoggle = document.querySelector('#startpausetoggle')
+const restart = document.querySelector('#restart')
 
-let second = 6
+let second = 400
 let timer = null
 
 function timeformat (s) {
-    const minutes = Math.floor(s / 60)
-    const remainingSeconds = second % 60
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    const minutes = Math.floor(s / 60) // Converts 60 seconds to 1 minute
+    const remainingSeconds = s   % 60
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`; // Format of the timer (00:00)
 }
 // initial display of the timer(5 seconds)
 timerdisplay.textContent = timeformat(second) 
@@ -15,9 +17,10 @@ timerdisplay.textContent = timeformat(second)
 
 // start the timer
 function startTimer () {
-    if (timer !== null) return 
+    startpausetoggle.classList.toggle('Pause')
+        if (timer !== null) return 
 
-    timer = setInterval(() => {
+        timer = setInterval(() => {
         timerdisplay.textContent = timeformat(second)
 
         // if second is set off to 0
@@ -30,19 +33,32 @@ function startTimer () {
     }, 1000) // code will run once every 1000ms(1s)
 }
 
-function restartTimer() {
-    clearInterval(timer)
-    timer = null
-    second = 6
-    timerdisplay.textContent = timeformat(second)
-    startTimer()
+function handleToggleChange () {
+    if (startpausetoggle.classList.contains('Pause')) {
+        startpausetoggle.textContent="Pause"
+        startTimer()
+    } else {
+        startpausetoggle.textContent="Resume"
+        pauseTimer()
+    }
 }
 
 function pauseTimer () {
+    clearInterval(timer)
+    timer = null
+}
 
+function restartTimer () {
+    second = 400
+    timerdisplay.textContent = timeformat(second) // to set it back from the original time
 }
 
 
 controls.addEventListener('click', (e) => {
-    if(e.target.id === 'startbtn') restartTimer()
+    if(e.target.id === 'startpausetoggle') {
+        handleToggleChange()
+    } 
+    if(e.target.id === 'restart') restartTimer()
 })
+
+// needs a variable that can control the flow
