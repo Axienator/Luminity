ea97918bffdb8ca8ab76deb0f35dda9031497ada
paullorenzopsima@gmail.com <jericho@gestafi.com>
2025-08-01 11:04:34 +0800

    Add short/long breaks

--- Assets/script.js
+++ Assets/script.js
@@ -3,13 +3,13 @@ const controls = document.querySelector('.control')
 const startpausetoggle = document.querySelector('#startpausetoggle')
 const restart = document.querySelector('#restart')
 
-let second = 400
+let second = 1500
 let timer = null
 
 function timeformat (s) {
     const minutes = Math.floor(s / 60) // Converts 60 seconds to 1 minute
     const remainingSeconds = s   % 60
-    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`; // Format of the timer (00:00)
+    return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}` // Format of the timer (00:00)
 }
 // initial display of the timer(5 seconds)
 timerdisplay.textContent = timeformat(second) 
@@ -17,10 +17,8 @@ timerdisplay.textContent = timeformat(second)
 
 // start the timer
 function startTimer () {
-    startpausetoggle.classList.toggle('Pause')
-        if (timer !== null) return 
-
-        timer = setInterval(() => {
+    if (timer !== null) return 
+    timer = setInterval(() => {
         timerdisplay.textContent = timeformat(second)
 
         // if second is set off to 0
@@ -30,10 +28,11 @@ function startTimer () {
         } else { // if not, continue reducing the seconds
             second-- 
         }
-    }, 1000) // code will run once every 1000ms(1s)
+    }, 950) // code will run once every 850ms
 }
 
 function handleToggleChange () {
+    startpausetoggle.classList.toggle('Pause')
     if (startpausetoggle.classList.contains('Pause')) {
         startpausetoggle.textContent="Pause"
         startTimer()
@@ -49,14 +48,16 @@ function pauseTimer () {
 }
 
 function restartTimer () {
-    second = 400
-    timerdisplay.textContent = timeformat(second) // to set it back from the original time
+    pauseTimer()
+    second = 1500
+    timerdisplay.textContent = timeformat(second)
+    startpausetoggle.classList.remove('Pause')
+    startpausetoggle.textContent = "Start"
 }
 
 
+
 controls.addEventListener('click', (e) => {
-    if(e.target.id === 'startpausetoggle') {
-        handleToggleChange()
-    } 
+    if(e.target.id === 'startpausetoggle') handleToggleChange()
     if(e.target.id === 'restart') restartTimer()
 })
